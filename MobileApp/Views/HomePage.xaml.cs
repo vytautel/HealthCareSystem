fdf5bf74d6b24fcec937cf817abdfed583aa5887
vytaute.lipeikaite@gmail.com <vytaute.lipeikaite@gmail.com>
2023-03-08 11:11:57 +0200

    Scroll, add symptom card (hiddes element under), user info 5 cards (tutorial)

--- MobileApp/Views/HomePage.xaml.cs
+++ MobileApp/Views/HomePage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using MobileApp.Components;
+﻿using Android;
+using Android.Views;
+using Android.Widget;
+using MobileApp.Components;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,20 +17,37 @@ namespace MobileApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        ObservableCollection<SymptomCard> cardItems;
         public HomePage()
         {
             InitializeComponent();
+            
+           // cardItems = new ObservableCollection<SymptomCard>();
+          //  myListView.ItemsSource = cardItems;
         }
 
         private void OnAddSymptomClicked(object sender, EventArgs e)
         {
             var newElement = new SymptomCard
             {
-                Title = "Card Title added", 
+                Title = "Card Title added",
                 Body = "Card Body Text added"
             };
 
-            pageView.Children.Add(newElement);
+            SymptomCardsLayout.Children.Add(newElement);
+
+            /* newElement.ButtonClickedCommand = new Command(() => {
+                 // Handle button click
+             });*/
+            // stringa baisiai
+            //cardItems.Add(newElement);
+
+            // Add the new button to the layout
+            //  myListView.Add(newElement);
+
+            // SymptomCards.FullScroll(FocusSearchDirection.Down);
+
+
         }
 
     }
