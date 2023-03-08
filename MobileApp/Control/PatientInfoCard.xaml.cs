828f4a11220b001debd94f6874dcd4abd5099542
vytaute.lipeikaite@gmail.com <vytaute.lipeikaite@gmail.com>
2023-03-08 13:45:17 +0200

    Attempts to fix binding (unsucessful)

--- MobileApp/Control/PatientInfoCard.xaml.cs
+++ MobileApp/Control/PatientInfoCard.xaml.cs
@@ -12,9 +12,36 @@ namespace MobileApp.Control
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PatientInfoCard : ContentView
     {
+        public static readonly BindableProperty IconProperty = BindableProperty.Create(
+            nameof(Icon),
+            typeof(string),
+            typeof(PatientInfoCard),
+            default(string)
+        );
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            nameof(Title),
+            typeof(string),
+            typeof(PatientInfoCard),
+            default(string)
+        );
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
         public PatientInfoCard()
         {
             InitializeComponent();
         }
     }
+
 }
